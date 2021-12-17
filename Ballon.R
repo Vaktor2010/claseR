3d355e62aadb079627a6923ba6b52bb1b97007e8
PruebasCREA <pruebas@GODOYCORDOBA.local>
2021-12-16 21:30:08 -0500

    nuevos datoss

--- Ballon.R
+++ Ballon.R
@@ -9,32 +9,53 @@ diplomado = separate(diplomado,"MARCADOR",c("MARCADOR_LOCAL","MARCADOR_VISITANTE
 str(diplomado$MARCADOR)  
 diplomado$MARCADOR_VISITANTE = substr(diplomado$MARCADOR_VISITANTE,2,2)  
 
-## Arreglo de las columnas
-#local = gsub("( )", "", diplomado$LOCAL)
-#visitante = gsub("( )", "", diplomado$VISITANTE)
-#summary(diplomado)
+
 
 names(diplomado)[1] <- "JUGADOR"
+names(diplomado)[13] <- "TARJETA_AMARILLA"
+names(diplomado)[14] <- "SEGUNDA_AMARILLA"
+names(diplomado)[15] <- "TARJETA_ROJA"
+names(diplomado)[16] <- "MIN_INICIAL"
+names(diplomado)[17] <- "MIN_FINAL"
+names(diplomado)[18] <- "TIEMPO_JUGADO"
 
 
-columnas = gsub (".","_",columnas)
-names(diplomado) = columnas
+partidos_jugados = diplomado %>% count(JUGADOR)
 
-names(diplomado$X.U.FEFF.JUGADOR) =
 
-  
 
-finalistas = diplomado %>% filter(NACIONALIDAD %in% c("Belgium","argentina")
+datos_nominados = diplomado %>% filter(JUGADOR %in% c("Kante","Jorginho","Lionel Messi","Karim Benzema","Robert Lewandowski",
+                                                      "Bruno Fernandes","Cesar Azpilicueta","Cristiano Ronaldo","Erling Haaland",
+                                                      "Gerard Moreno","Gianluigi Donnarumma","Giorgio Chiellini","Harry Kane",
+                                                      "Kevin De Bruyne","Kylian Mbappe","Lautaro Martinez","Leonardo Bonucci",
+                                                      "Luis Suarez","Luka Modric","Mason Mount","Mohamed Salah","Neymar","Pedri",
+                                                      "Nicolo Barella","Phil Foden","Raheem Sterling","Riyad Mahrez","Romelu Lukaku",
+                                                      "Ruben Dias","Simon Kjaer")) %>% group_by(JUGADOR)%>%summarise (TOTAL_GOLES = sum(GOLES),TOTAL_ASISTENCIAS = sum(ASISTENCIAS),TOTAL_TA = sum(TARJETA_AMARILLA),TOTAL_STA = sum(SEGUNDA_AMARILLA),
+                                                      TOTAL_ROJAS = sum(TARJETA_ROJA),TOTAL_TIEMPO_JUGADO = sum(TIEMPO_JUGADO))
 
-promedio_minutos_jugados = mean(TIEMPO..JUGADO)
+
+promedio_gol = data.frame(partidos_jugados$n / datos_nominados$TOTAL_GOLES)
+
+
+finalistas = diplomado %>% filter(JUGADOR %in% c("Kante","Jorginho","Lionel Messi","Karim Benzema","Robert Lewandowski"))
+
+
+
+
+
+
 
 
 goles_messi = diplomado %>% filter(JUGADOR == "Lionel Messi") %>%
-  filter(GOLES > 0) %>% group_by(JUGADOR) %>%  summarise(promedio_minutos_jugados = mean(TIEMPO..JUGADO))
+  filter(GOLES > 0) %>% group_by(JUGADOR) %>%  summarise (promedio_minutos_jugados = mean(TIEMPO_JUGADO))
 
-gol = colSums (goles_messi[ , 3:6])
-gol
- 
+goles_fin = diplomado %>% filter(JUGADOR %in% c("Kante","Jorginho","Lionel Messi","Karim Benzema","Robert Lewandowski")) %>%
+  filter(GOLES > 0) %>% group_by(JUGADOR)%>%  summarise (promedio_minutos_jugados = sum(TIEMPO_JUGADO))
+
+
+
+
+
 
 
 
